7657f806309452aa5f4f01d7050a69cde845326e
badbreze <badbreze@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-16 07:45:08 +0000

    Remove default timezone
    git-svn-id: https://plugins.svn.wordpress.org/twitter-posts-to-blog/trunk@773124 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitter-posts-to-blog.php
+++ twitter-posts-to-blog.php
@@ -2,11 +2,9 @@
 /*
 Plugin Name: Twitter posts to Blog
 Description: Post twetts to your blog
-Version: 1.4.6
+Version: 1.4.6.1
 Author: Damian Gomez
 */
-date_default_timezone_set('UTC');
-
 
 //require_once 'libs/tmhOAuth/tmhOAuth.php';
 //require_once 'libs/tmhOAuth/tmhUtilities.php';
@@ -23,7 +21,7 @@ $tokens_error = false;
 //Actions
 add_action('wp_loaded',						'dg_tw_options');
 add_action('dg_tw_event_start',				'dg_tw_load_next_items');
-add_action('wp_ajax_dg_tw_event_start',				'dg_tw_load_next_items');
+add_action('wp_ajax_dg_tw_event_start',		'dg_tw_load_next_items');
 add_action('admin_menu',					'dg_add_menu_item');
 add_action('loop_start',					'dg_tw_loop_start');
 add_action('admin_notices',					'dg_tw_feedback' );
@@ -31,6 +29,12 @@ add_action('wp_ajax_dg_tw_manual_publish',	'dg_tw_manual_publish');
 
 //Filters
 add_filter('cron_schedules','dg_tw_schedule');
+
+
+function myplugin_init() {
+	load_plugin_textdomain('twitter-posts-to-blog', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+add_action('plugins_loaded', 'myplugin_init');
 
 //Hooks
 register_activation_hook( __FILE__, 'dg_tw_activation' );
